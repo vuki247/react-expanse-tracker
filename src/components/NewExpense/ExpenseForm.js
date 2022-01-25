9264848b1151b9f60de6e6ba5bcb909d463a8138
vuki247 <mateo.vukovic@tutamail.com>
2022-01-25 10:13:56 +0100

    Conditional content - Challenge

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -65,6 +65,9 @@ function ExpenseForm(props) {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add expense</button>
       </div>
     </form>
